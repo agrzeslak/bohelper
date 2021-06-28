bec99e0f99e60dd74f8c5ccd0dbbe878fb60458a
Andrzej Grzeslak <agrzeslak0@gmail.com>
2021-06-28 21:58:27 +1000

    Further progress.
    Program is now runnable, with functionality working (including associated tests) until the bad characters state.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 use std::process;
 
 fn main() {
-    // let pattern = bohelper::generate_pattern(12500000);
-    // println!("{}", pattern);
-
-    // let offset =
-    // bohelper::get_pattern_offset("42397042", bohelper::generate_pattern(3000).as_str())
-    // .unwrap();
-
-    // println!("{}", offset);
-
-    // bohelper::run().unwrap_or_else(|err| {
-    //     eprintln!("Error encountered: {}", err);
-    //     process::exit(1);
-    // });
+    bohelper::run().unwrap_or_else(|err| {
+        eprintln!("Error encountered: {}", err);
+        process::exit(1);
+    });
 }
